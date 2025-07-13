079d87c129c22a9779285265ac763f2dca55d722
Marq Claire <marqclaire@Marqs-MacBook-Air.local>
2025-07-13 13:57:47 -0400

    Update NET version in .yml to 9

--- apps/api-server/ApiServer/obj/Debug/net9.0/ApiServer.AssemblyInfo.cs
+++ apps/api-server/ApiServer/obj/Debug/net9.0/ApiServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5483e5f1cd946a4589251048200e925570710c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6586c4bdbe74937f6e0edda0b893210fe7cc020")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
